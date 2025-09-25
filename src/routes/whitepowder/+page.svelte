525b4da8d71ceb9ab77893699dab72103ab4ff03
kurosiko <kurosiko@outlook.com>
2025-09-26 00:26:45 +0900

    add video player

---
+++ src/routes/whitepowder/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    let video_idx = $state(0);
+    const src_list = ["/lamunation.webm","/krmn.mp4"];
+</script>
+<div class="flex flex-col justify-center content-center items-center overflow-scroll bg-white *:inset-0 *:size-full">
+    {#each src_list as video}
+        <video muted controls>
+            <source src={video}/>
+        </video>
+    {/each}
+</div>
