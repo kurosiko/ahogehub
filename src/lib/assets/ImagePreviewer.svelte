612c68ad8a63d00ac7e8494aab5406decbd7741d
kurosiko <kurosiko@outlook.com>
2025-11-30 11:36:13 +0900

    add explorer page(WIP)

--- src/lib/assets/ImagePreviewer.svelte
+++ src/lib/assets/ImagePreviewer.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
     const {src,onclick} = $props()
 </script>
-<div class="size-50 hover:scale-110 transition-all ease-in-out group relative hover:z-10">
+<div class="size-50 hover:scale-110 transition-all ease-in-out group relative hover:z-1 hover:bg-neutral-700">
     <img
         src={src} alt="ahoge"
-        class="w-full group-hover:brightness-50 object-center"
+        class="size-full group-hover:brightness-30 object-center object-contain"
     >
     <button
-        class="absolute opacity-0 group-hover:opacity-100 bottom-1/2 translate-y-1/2 transition-opacity w-full text-center outline p-5"
+        class="absolute opacity-0 group-hover:opacity-100 bottom-1/2 translate-y-1/2 transition-opacity w-full text-center outline p-5 size-full"
         onclick={()=>onclick(src)}
     >Detail</button>
 </div>
--- src/routes/explore/+page.svelte
+++ src/routes/explore/+page.svelte
@@ -1,91 +1,79 @@
 <script lang="ts">
     import ImagePreviewer from "$lib/assets/ImagePreviewer.svelte";
-    const ahogeList = [
-      {
-        id: 1,
-        name: 'Hoshino',
-        image: '/ahoge_sample/hoshino.webp',
-        description: 'A beautiful ahoge.'
-      },
-      {
-        id: 2,
-        name: 'Hoshino V2',
-        image: '/ahoge_sample/hoshino_v2.webp',
-        description: 'A beautiful ahoge.'
-      },
-      {
-        id: 3,
-        name: 'Maki',
-        image: '/ahoge_sample/maki.webp',
-        description: 'A beautiful ahoge.'
-      },
-      {
-        id: 4,
-        name: 'Uzawa',
-        image: '/ahoge_sample/uzawa.webp',
-        description: 'A beautiful ahoge.'
-      },
-      {
-        id: 5,
-        name: 'Long Ago',
-        image: '/ahoge_sample/long_ago.webp',
-        description: 'A beautiful ahoge.'
-      }
-    ]
+  import { cubicInOut } from "svelte/easing";
+  import { fade, fly, slide } from "svelte/transition";
+
     let sidebar_opened = $state(false)
     let selected_item:{
       image:string,
       description:string,
       source:string
     } = $state({
-      image:"test",
-      description:"test",
-      source:"test"
+      image:"ahogetic default value",
+      description:"ahogetic default value",
+      source:"ahogetic default value"
     })
     const callback = (data:any)=>{
       selected_item.image = data
       sidebar_opened = true
     }
     let files = $state([])
+    const api_request = async ()=>{
+      const response = await fetch("https://api.kurosiko.com/get",{
+        method:"GET",
+      });
+      if (response.status != 200) return
+      const body:{files:string[]} = await response.json()
+      return body
+    }
 </script>
 
-<h1 class="text-center text-4xl top-0 z-500">
-    Explore
-</h1>
-<div class="md:flex flex-row gap-5 p-5 relative">
-    <div class="md:flex flex-1 hidden">
-        <div class="w-full outline-1 bg-red-700 flex">
-            <div>
+
+<h1 class="text-center text-4xl top-0 z-500 py-5 font-semibold">Explore</h1>
+<div class="relative">
+    <div class="md:flex flex-row gap-5 p-5 transition-all">
+      <div class="md:flex flex-1 hidden">
+          <div class="w-full flex">
+              <div>
                 <ul>
-                    <li>Attributes 1</li><li>Attributes 1</li><li>Attributes 1</li><li>Attributes 1</li>
-                </ul>
+                  <li>Attributes 1</li><li>Attributes 1</li><li>Attributes 1</li><li>Attributes 1</li>
+              </ul>
             </div>
-        </div>
-    </div>   
-    <div class="md:flex-5 bg-blue-500 ">
-       {#each files as image} 
-          <ImagePreviewer src={"https://api.kurosiko.com/item/"+image} onclick={callback}/>
-        {/each}
-        
-    </div>
-    {#if sidebar_opened}
-      <div class="md:flex-1 sticky top-0 left-0 text-center">
+          </div>
+      </div>   
+      <div class="md:flex-5 grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))]">
+      {#await api_request()}
+        <h1>Loading!</h1>
+      {:then res} 
+        {#if res && res.files && res.files.length}
+          {#each res.files as file}
+            <ImagePreviewer src={"https://api.kurosiko.com/item/"+file} onclick={callback}/>
+          {/each}
+        {:else}
+          <p>No files</p>
+        {/if}
+      {/await}
+      </div>
+      {#if sidebar_opened}
+      <div class="md:flex-2 sticky top-0 left-0 text-cente w-full" transition:fly|global={{duration:500,x:500,easing:cubicInOut}}>
         <img
-          src={selected_item.image} alt={selected_item.description}
+          src={selected_item.image} alt={selected_item.description + "is loading"}
           class="w-full"
         >
-        <button onclick={()=>{sidebar_opened = false}}>close</button>
+        <div class="flex flex-col gap-2.5 py-2.5 ">
+          <p class="text-lg">Source:</p>
+          <p>{selected_item.source}</p>
+          <p class="text-lg">Creator</p>
+          <p>{"ahoge lover"}</p>
+          <p class="text-lg">Link:</p>
+          <p><a href={selected_item.image} class="underline hover:text-white/50">{selected_item.image}</a></p>
+          <p class="text-lg">Description:</p>
+          <p>{selected_item.description}</p>
+          <p class="text-lg">Uploader:</p>
+          <p>{"ahoge lover"}</p>
+          <button onclick={()=>{sidebar_opened = false}} class="text-center mx-auto text-lg font-base">close</button>
+        </div>
       </div>
-    {/if}
-    <button onclick={()=>{
-      fetch("https://api.kurosiko.com/get").then((v)=>{
-        v.json().then((j)=>{
-          console.log(j)
-          files = j.files
-        })
-      })
-    }}>TEST</button>
+      {/if}
+  </div>
 </div>
-
-
-
